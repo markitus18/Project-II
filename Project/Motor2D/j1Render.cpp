b02e8eeb073f9970f7411318219600b0d51331a0
markitus18 <marc18.garrigo@gmail.com>
2016-02-17 18:41:19 +0100

    Mixing renders

--- Project/Motor2D/j1Render.cpp
+++ Project/Motor2D/j1Render.cpp
@@ -1,7 +1,6 @@
-#include "j1Render.h"
-
 #include "j1App.h"
 #include "j1Window.h"
+#include "j1Render.h"
 #include "j1Console.h"
 
 #define VSYNC true
@@ -27,7 +26,7 @@ bool j1Render::Awake(pugi::xml_node& config)
 	// load flags
 	Uint32 flags = SDL_RENDERER_ACCELERATED;
 
-	if(config.child("vsync").attribute("value").as_bool(true) == true)
+	if (config.child("vsync").attribute("value").as_bool(true) == true)
 	{
 		flags |= SDL_RENDERER_PRESENTVSYNC;
 		LOG("Using vsync");
@@ -35,7 +34,7 @@ bool j1Render::Awake(pugi::xml_node& config)
 
 	renderer = SDL_CreateRenderer(App->win->window, -1, flags);
 
-	if(renderer == NULL)
+	if (renderer == NULL)
 	{
 		LOG("Could not create the renderer! SDL_Error: %s\n", SDL_GetError());
 		ret = false;
@@ -48,8 +47,10 @@ bool j1Render::Awake(pugi::xml_node& config)
 		camera.y = 0;
 	}
 
-	App->console->AddCVar("camera_x", &camera.x, this, false);
-	App->console->AddCVar("camera_y", &camera.y, this, false);
+	App->console->AddCVar("camera_x", &camera.x);
+	App->console->AddCVar("camera_y", &camera.y);
+
+	SDL_RenderGetViewport(renderer, &viewport);
 
 	return ret;
 }
@@ -59,7 +60,7 @@ bool j1Render::Start()
 {
 	LOG("render start");
 	// back background
-	SDL_RenderGetViewport(renderer, &viewport);
+
 	return true;
 }
 
@@ -138,10 +139,18 @@ bool j1Render::Blit(const SDL_Texture* texture, int x, int y, const SDL_Rect* se
 	uint scale = App->win->GetScale();
 
 	SDL_Rect rect;
-	rect.x = (int)(camera.x * speed) + x * scale;
-	rect.y = (int)(camera.y * speed) + y * scale;
+	if (useCamera)
+	{
+		rect.x = (int)(camera.x * speed) + x * scale;
+		rect.y = (int)(camera.y * speed) + y * scale;
+	}
+	else
+	{
+		rect.x = (int)x * scale;
+		rect.y = (int)y * scale;
+	}
 
-	if(section != NULL)
+	if (section != NULL)
 	{
 		rect.w = section->w;
 		rect.h = section->h;
@@ -157,7 +166,7 @@ bool j1Render::Blit(const SDL_Texture* texture, int x, int y, const SDL_Rect* se
 	SDL_Point* p = NULL;
 	SDL_Point pivot;
 
-	if(pivot_x != INT_MAX && pivot_y != INT_MAX)
+	if (pivot_x != INT_MAX && pivot_y != INT_MAX)
 	{
 		pivot.x = pivot_x;
 		pivot.y = pivot_y;
@@ -173,7 +182,66 @@ bool j1Render::Blit(const SDL_Texture* texture, int x, int y, const SDL_Rect* se
 	return ret;
 }
 
-bool j1Render::DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool filled, bool use_camera) const
+bool j1Render::Blit(const SDL_Texture* texture, const SDL_Rect* onScreenPosition, const SDL_Rect* section, float speed, double angle, int pivot_x, int pivot_y) const
+{
+	bool ret = true;
+	uint scale = App->win->GetScale();
+
+	SDL_Rect rect;
+	if (useCamera)
+	{
+		rect.x = (int)(camera.x * speed) + onScreenPosition->x * scale;
+		rect.y = (int)(camera.y * speed) + onScreenPosition->y * scale;
+	}
+	else
+	{
+		rect.x = (int)onScreenPosition->x * scale;
+		rect.y = (int)onScreenPosition->y * scale;
+	}
+	rect.w = onScreenPosition->w;
+	rect.h = onScreenPosition->h;
+
+	if (onScreenPosition->w == 0 && onScreenPosition->h == 0)
+	{
+		if (section != NULL)
+		{
+			rect.w = section->w;
+			rect.h = section->h;
+		}
+		else
+		{
+			SDL_QueryTexture((SDL_Texture*)texture, NULL, NULL, &rect.w, &rect.h);
+		}
+	}
+	else
+	{
+		rect.w = onScreenPosition->w;
+		rect.h = onScreenPosition->h;
+	}
+
+	rect.w *= scale;
+	rect.h *= scale;
+
+	SDL_Point* p = NULL;
+	SDL_Point pivot;
+
+	if (pivot_x != INT_MAX && pivot_y != INT_MAX)
+	{
+		pivot.x = pivot_x;
+		pivot.y = pivot_y;
+		p = &pivot;
+	}
+
+	if (SDL_RenderCopyEx(renderer, (SDL_Texture*)texture, section, &rect, angle, p, SDL_FLIP_NONE) != 0)
+	{
+		LOG("Cannot blit to screen. SDL_RenderCopy error: %s", SDL_GetError());
+		ret = false;
+	}
+
+	return ret;
+}
+
+bool j1Render::DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool filled, bool ignoreCamera) const
 {
 	bool ret = true;
 	uint scale = App->win->GetScale();
@@ -182,17 +250,20 @@ bool j1Render::DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a
 	SDL_SetRenderDrawColor(renderer, r, g, b, a);
 
 	SDL_Rect rec(rect);
-	if(use_camera)
+	rec.x = (int)(rect.x * scale);
+	rec.y = (int)(rect.y * scale);
+	rec.w *= scale;
+	rec.h *= scale;
+	if (useCamera)
 	{
-		rec.x = (int)(camera.x + rect.x * scale);
-		rec.y = (int)(camera.y + rect.y * scale);
-		rec.w *= scale;
-		rec.h *= scale;
+		rec.x += camera.x;
+		rec.y += camera.y;
 	}
+
 
 	int result = (filled) ? SDL_RenderFillRect(renderer, &rec) : SDL_RenderDrawRect(renderer, &rec);
 
-	if(result != 0)
+	if (result != 0)
 	{
 		LOG("Cannot draw quad to screen. SDL_RenderFillRect error: %s", SDL_GetError());
 		ret = false;
@@ -201,7 +272,7 @@ bool j1Render::DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a
 	return ret;
 }
 
-bool j1Render::DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool use_camera) const
+bool j1Render::DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool ignoreCamera) const
 {
 	bool ret = true;
 	uint scale = App->win->GetScale();
@@ -211,12 +282,12 @@ bool j1Render::DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 
 
 	int result = -1;
 
-	if(use_camera)
+	if (useCamera)
 		result = SDL_RenderDrawLine(renderer, camera.x + x1 * scale, camera.y + y1 * scale, camera.x + x2 * scale, camera.y + y2 * scale);
 	else
 		result = SDL_RenderDrawLine(renderer, x1 * scale, y1 * scale, x2 * scale, y2 * scale);
 
-	if(result != 0)
+	if (result != 0)
 	{
 		LOG("Cannot draw quad to screen. SDL_RenderFillRect error: %s", SDL_GetError());
 		ret = false;
@@ -225,7 +296,7 @@ bool j1Render::DrawLine(int x1, int y1, int x2, int y2, Uint8 r, Uint8 g, Uint8 
 	return ret;
 }
 
-bool j1Render::DrawCircle(int x, int y, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool use_camera) const
+bool j1Render::DrawCircle(int x, int y, int radius, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool ignoreCamera) const
 {
 	bool ret = true;
 	uint scale = App->win->GetScale();
@@ -233,17 +304,12 @@ bool j1Render::DrawCircle(int x, int y, int radius, Uint8 r, Uint8 g, Uint8 b, U
 	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 	SDL_SetRenderDrawColor(renderer, r, g, b, a);
 
-	if (use_camera)
-	{
-		x += camera.x;
-		y += camera.y;
-	}
 	int result = -1;
 	SDL_Point points[360];
 
 	float factor = (float)M_PI / 180.0f;
 
-	for(uint i = 0; i < 360; ++i)
+	for (uint i = 0; i < 360; ++i)
 	{
 		points[i].x = (int)(x + radius * cos(i * factor));
 		points[i].y = (int)(y + radius * sin(i * factor));
@@ -251,7 +317,7 @@ bool j1Render::DrawCircle(int x, int y, int radius, Uint8 r, Uint8 g, Uint8 b, U
 
 	result = SDL_RenderDrawPoints(renderer, points, 360);
 
-	if(result != 0)
+	if (result != 0)
 	{
 		LOG("Cannot draw quad to screen. SDL_RenderFillRect error: %s", SDL_GetError());
 		ret = false;
