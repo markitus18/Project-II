5b3b62d162379bda9df2f2ac56a4749ff6cf75fe
Vulpem <davidher96@gmail.com>
2016-04-03 17:10:05 +0200

    Name fix, clearer

--- Project/Motor2D/j1App.cpp
+++ Project/Motor2D/j1App.cpp
@@ -256,14 +256,13 @@ void j1App::FinishUpdate()
 		last_sec_frame_count = 0;
 	}
 
-	float avg_fps = float(frame_count) / startup_time.ReadSec();
 	float seconds_since_startup = startup_time.ReadSec();
 	uint32 last_frame_ms = frame_time.Read();
 	uint32 frames_on_last_update = prev_last_sec_frame_count;
 
 	static char title[256];
-	sprintf_s(title, 256, "Av.FPS: %.2f Last Frame Ms: %u Last sec frames: %i Last dt: %.3f Time since startup: %.3f Frame Count: %lu ",
-			  avg_fps, last_frame_ms, frames_on_last_update, dt, seconds_since_startup, frame_count);
+	sprintf_s(title, 256, "Last sec frames: %i Last Frame Ms: %u  Last dt: %.3f Time since startup: %.3f",
+		frames_on_last_update, last_frame_ms, dt, seconds_since_startup);
 	App->win->SetTitle(title);
 
 	if(capped_ms > 0 && (int)last_frame_ms < capped_ms)
