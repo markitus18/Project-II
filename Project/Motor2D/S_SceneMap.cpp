e08299a87e0e4747b9dc59b2735e6340f3f5e213
Vulpem <davidher96@gmail.com>
2016-04-04 21:08:26 +0200

    Fixed for every scale

--- Project/Motor2D/S_SceneMap.cpp
+++ Project/Motor2D/S_SceneMap.cpp
@@ -272,8 +272,8 @@ void S_SceneMap::ManageInput(float dt)
 		}
 
 	//---------------------------------------------------------------------
-		CAP(App->render->camera.x, 0, 8000);
-		CAP(App->render->camera.y, 0, 8000);
+		CAP(App->render->camera.x, 0, 2433*App->win->GetScale());
+		CAP(App->render->camera.y, 0, 2700 * App->win->GetScale());
 
 }
 
