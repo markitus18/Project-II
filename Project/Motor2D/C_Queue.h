395560c518198db9b9d5d1daf811e7dc7b853002
Jconrega <xema11.1.96@gmail.com>
2016-04-06 13:33:41 +0200

    queue

--- Project/Motor2D/C_Queue.h
+++ Project/Motor2D/C_Queue.h
@@ -6,21 +6,36 @@
 class My_Queue
 {
 public:  
-	j1Timer* time;
+	std::queue<float> mytime;
 	
 	std::queue<PlayerData*> myqueue;
 
+	j1Timer inicial_time;
 
 	My_Queue();
-	My_Queue(j1Timer _time){ time = _time; }
 
 	~My_Queue();
 
-	void add(PlayerData* player, j1Timer _time)
+	void add(PlayerData* player, float _time)
 	{
-		myqueue.push()
+		myqueue.push(player);
+		mytime.push(_time);
+
 	}
 
+	bool is_Created()
+	{
+		float new_time = mytime.back();
+		if (new_time == inicial_time.ReadSec())
+		{
+			myqueue.back();
+			return true;
+		}
+		return false;
+	}
+
+	
+	
 private:
 
 };
