85333ec8e8e91c57e38c919701c223cbf9dab455
Vulpem <davidher96@gmail.com>
2016-04-11 19:14:10 +0200

    Starting bases

--- Project/Motor2D/M_IA.cpp
+++ Project/Motor2D/M_IA.cpp
@@ -1,9 +1,67 @@
 #include "M_IA.h"
+#include "j1App.h"
 
+//General Base ---------------------------------------------------------------------------------------------------------------
+
+bool Base::BaseUpdate(float dt)
+{
+	if (spawning)
+	{
+		generationTimer += dt;
+		if (generationTimer >= generationDelay)
+		{
+			generationTimer = 00.f;
+			Spawn();
+		}
+	}
+	PersonalUpdate(dt);
+	return IsBaseAlive();
+}
+
+bool Base::PersonalUpdate(float dt)
+{
+
+}
+
+void Base::Spawn()
+{
+	Unit* tmp = App->entityManager->CreateUnit(spawningPoint.x, spawningPoint.y, typeOfBase, COMPUTER);
+	unitsInBase.push_back(tmp);
+}
+
+bool Base::IsBaseAlive()
+{
+	bool ret = false;
+	if (!buildings.empty())
+	{
+		ret = true;
+	}
+	else if (spawning)
+	{
+		spawning = false;
+	}
+
+	if (!ret && !unitsInBase.empty())
+	{
+		ret = true;
+	}
+	if (!ret && !unitsOutOfBase.empty())
+	{
+		ret = true;
+	}
+	return ret;
+}
+
+
+
+
+
+
+//Module IA ------------------------------------------------------------------------------------------------------------------
 
 M_IA::M_IA(bool start_enabled) : j1Module(start_enabled)
 {
-	name.create("Missil");
+	name.create("IA");
 }
 
 bool M_IA::Start()
@@ -13,10 +71,28 @@ bool M_IA::Start()
 
 bool M_IA::Update(float dt)
 {
+	std::vector<Base>::iterator it = basesList.begin();
+	while (it != basesList.end())
+	{
+		if (it->BaseUpdate(dt) == false)
+		{
+			LOG("A base was erradicated :D");
+			std::vector<Base>::iterator it2 = it;
+			it2++;
+			basesList.erase(it);
+			it = it2;
+		}
+		else
+		{
+			it++;
+		}
+	}
+
 	return true;
 }
 
 bool M_IA::CleanUp()
 {
+	//Cleaning Bases
 	return true;
 }
--- Project/Motor2D/M_IA.h
+++ Project/Motor2D/M_IA.h
@@ -2,6 +2,30 @@
 #define __IA_H__
 
 #include "j1Module.h"
+#include "M_EntityManager.h"
+
+class Base
+{
+public:
+	bool BaseUpdate(float dt);
+
+private:
+	virtual bool PersonalUpdate(float dt);
+
+	void Spawn();
+	bool IsBaseAlive();
+
+	std::list<Unit*>		unitsInBase;
+	std::list<Unit*>		unitsOutOfBase;
+	std::list<Building*>	buildings;
+
+	bool  spawning;
+	float generationTimer;
+	float generationDelay;
+
+	iPoint spawningPoint;
+	Unit_Type typeOfBase;
+};
 
 class M_IA : public j1Module
 {
@@ -17,6 +41,8 @@ public:
 
 private:
 
+	std::vector<Base> basesList;
+
 };
 
-#endif //__MISSIL_H__
+#endif 
