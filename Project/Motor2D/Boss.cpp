69bbe099a0b65bcd88a0626412386ab0bfc49e34
GottaCodeHarder <masterkonnen@gmail.com>
2016-04-26 15:02:30 +0100

    Boss - Second Commit

--- Project/Motor2D/Boss.cpp
+++ Project/Motor2D/Boss.cpp
@@ -17,4 +17,110 @@
 #include "M_Map.h"
 #include "Building.h"
 
-// Shit this is complicated
+Boss::Boss() : Unit()
+{
+
+}
+
+Boss::~Boss()
+{
+
+}
+
+bool Boss::Start()
+{
+
+	movement_state = MOVEMENT_IDLE;
+	in_combatTimer.Start();
+	shieldTimer.Start();
+
+	App->entityManager->UpdateCurrentFrame(this);
+
+	return true;
+}
+
+bool Boss::Update(float dt)
+{
+	bool ret = true;
+	bool collided = false;
+
+	if (waitingForPath)
+	{
+		if (!path.empty())
+		{
+			//GetNewTarget();
+			waitingForPath = false;
+		}
+	}
+
+	//General state machine
+	if (movement_state == MOVEMENT_WAIT)
+	{
+		switch (bossState)
+		{
+		case (BOSS_STAND) :
+		{
+							  //Stop();
+							  break;
+		}
+		case(BOSS_MOVE) :
+		{
+							//Stop();
+							break;
+		}
+		case(BOSS_ATTACK) :
+		{
+							  //UpdateAttackState(dt);
+							  break;
+		}
+		}
+
+		//Movement state machine
+		switch (movement_state)
+		{
+		case (MOVEMENT_MOVE) :
+		{
+								 //UpdateMovement(dt);
+								 break;
+		}
+		case (MOVEMENT_ATTACK_IDLE) :
+		{
+										//UpdateAttack(dt);
+										break;
+		}
+		case (MOVEMENT_ATTACK_ATTACK) :
+		{
+										  //UpdateAttack(dt);
+										  break;
+		}
+		case (MOVEMENT_DIE) :
+		{
+								//ret = UpdateDeath(dt);
+								break;
+		}
+		}
+
+		/*if (state != STATE_GATHER && state != STATE_GATHER_RETURN && state != STATE_MOVE && state != STATE_STAND)
+		{
+		if (gatheringResource)
+		{
+		if (gatheringResource->gatheringUnit)
+		{
+		gatheringResource->gatheringUnit = NULL;
+		}
+		gatheringResource = NULL;
+		}
+		}*/
+
+		if (bossState != STATE_DIE)
+		{
+			RegenShield();
+			CheckMouseHover();
+		}
+		if (sprite.texture)
+		{
+			Draw(dt);
+		}
+		return ret;
+	}
+}
--- Project/Motor2D/Boss.h
+++ Project/Motor2D/Boss.h
@@ -10,9 +10,76 @@
 
 #include "j1Timer.h"
 
-class Boss : public Unit
+enum Boss_State
 {
+	BOSS_STAND,
+	BOSS_MOVE,
+	BOSS_ATTACK,
+	BOSS_SPELL,
+	BOSS_STUN,
+	BOSS_DIE,
+};
 
+enum Boss_Attack_State
+{
+	BOSS_ATK_STAND,
+	BOSS_ATK_DEFEND,
+	BOSS_ATK_ATTACK,
+	BOSS_ATK_COOLDOWN,
+};
+
+class Boss : protected Unit
+{
+public:
+	Boss();
+	Boss(float x, float y, Unit_Type _type, Player_Type);
+	Boss(fPoint);
+	~Boss();
+
+	bool Start();
+	bool Update(float dt);
+private:
+
+	//Movement functions --------------------------
+	void UpdateMovement(float dt);
+	bool UpdatePosition(float dt);
+	bool UpdateVelocity(float dt);
+
+	//void UpdateBarPosition();
+
+	void GetDesiredVelocity();
+
+	bool Rotate(float dt);
+
+	bool GetNewTarget();
+
+	bool isTargetReached();
+	bool isAngleReached();
+	//---------------------------------------------
+
+	//Attack functions ----------------------------
+	void UpdateAttackState(float dt);
+	void UpdateAttack(float dt);
+	// --------------------------------------------
+
+	bool UpdateDeath(float dt);
+
+	void LoadLibraryData();
+
+private:
+
+	Boss_State bossState = BOSS_STAND;
+	Boss_Attack_State bossAtkState = BOSS_ATK_ATTACK;
+
+	//Movement variables--------------------------
+	iPoint target;
+	//Velocities
+	C_Vec2<float> currentVelocity = { 0, 0 };
+	C_Vec2<float> desiredVelocity = { 0, 0 };
+
+	float rotationSpeed = 500.0f; //Used as angles / seconds
+	float targetRadius = 3.0f;
+	//--------------------------------------------
 };
 
 #endif
