55c9f333d99a77da27d5d945ccc78a78159c9393
markitus18 <marc18.garrigo@gmail.com>
2016-04-26 15:54:55 +0100

    starting with s-mpa

--- Project/Motor2D/j1App.cpp
+++ Project/Motor2D/j1App.cpp
@@ -46,8 +46,8 @@ j1App::j1App(int argc, char* args[]) : argc(argc), args(args)
 	explosion = new M_Explosion(false);
 
 	//Scenes-------------------------false
-	sceneMap = new S_SceneMap(false);
-	sceneMenu = new S_SceneMenu(true);
+	sceneMap = new S_SceneMap(true);
+	sceneMenu = new S_SceneMenu(false);
 	//-------------------------------
 
 	fs = new M_FileSystem(true);
