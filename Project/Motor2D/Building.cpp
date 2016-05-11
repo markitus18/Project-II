f2726bbdba3b02ec1efc612c8b59ce6826cc9f81
markitus18 <marc18.garrigo@gmail.com>
2016-05-12 00:39:18 +0200

    Revert "Nvidia please"
    This reverts commit 90f353778380d1ff26af84525b1012640826ff9d.

--- Project/Motor2D/Building.cpp
+++ Project/Motor2D/Building.cpp
@@ -432,7 +432,7 @@ void Building::AddNewUnit(Unit_Type unitType, int creationTime, int unitPsi)
 
 Unit* Building::CreateUnit(Unit_Type type, Player_Type controller)
 {
-	iPoint pos = FindCloseWalkableTile(2, 6);
+	iPoint pos = FindCloseWalkableTile();
 	iPoint dst = App->pathFinding->MapToWorld(pos.x, pos.y);
 	return App->entityManager->CreateUnit(dst.x, dst.y, type, controller, this);
 }
@@ -545,17 +545,17 @@ void Building::FinishSpawn()
 	App->entityManager->FinishBuildingSpawn(this);
 }
 
-iPoint Building::FindCloseWalkableTile(int correction_x, int correction_y)
+iPoint Building::FindCloseWalkableTile()
 {
-	iPoint tile = { (int)position.x - 1 + correction_x, (int)position.y - 1 + correction_y };
+	iPoint tile = { (int)position.x - 1, (int)position.y - 1 };
 	bool maxL = false, maxD = false, maxR = false, maxU = false;
 	while (!App->pathFinding->IsWalkable(tile.x, tile.y))
 	{
-		if (!maxR)
+		if (!maxL)
 		{
-			tile.x--;
-			if (tile.x == position.x - 1)
-				maxR = true;
+			tile.x++;
+			if (tile.x == position.x + width_tiles + 1)
+				maxL = true;
 		}
 		else if (!maxD)
 		{
@@ -563,11 +563,11 @@ iPoint Building::FindCloseWalkableTile(int correction_x, int correction_y)
 			if (tile.y == position.y + height_tiles + 1)
 				maxD = true;
 		}
-		else if (!maxL)
+		else if (!maxR)
 		{
-			tile.x++;
-			if (tile.x == position.x + width_tiles + 1)
-				maxL = true;
+			tile.x--;
+			if (tile.x == position.x - 1)
+				maxR = true;
 		}
 		else if (!maxU)
 		{
