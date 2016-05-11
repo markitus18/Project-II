715d119e484802de8342892d70e1d3caa4b3a05b
David Roig <roig_96@hotmail.com>
2016-05-11 17:05:26 +0200

    Enemy unit selection bug fixed

--- Project/Motor2D/M_GUI.cpp
+++ Project/Motor2D/M_GUI.cpp
@@ -305,6 +305,7 @@ void M_GUI::UI_Unselect()
 
 void M_GUI::UI_UnitSelect(const Unit* _unit)
 {
+	if (_unit->race == PROTOSS)
 	App->sceneMap->statsPanel_m->SelectUnit(_unit);
 	//App->sceneMap->statsPanel_m->setStatsWireframesMult(_unit);
 }
